ba485a5cdfe05648a48afae818f4a4b8a3981c54
Marco Stellin <letal32@gmail.com>
2018-02-27 21:55:34 +0000

    Addin new countdowns and set the initial value

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,28 +1,45 @@
 var date = document.querySelector("input[type=date]");
-var years = document.querySelector(".years");
-var months = document.querySelector(".months");
-var days = document.querySelector(".days");
-var hours = document.querySelector(".hours");
-var minutes = document.querySelector(".minutes");
-var seconds = document.querySelector(".seconds");
+var newCntdownBtn = document.querySelector("#newCountdown"); 
+var cntdownContainer = document.querySelector("#container");
 
-var countdown = moment.duration();
+var countdownIds = [0,1,2,3,4,5,6,7,8];
+var cntdowns = [];
 
+newCntdownBtn.addEventListener("click", function addCounter(){
+    var cntdownDiv = document.createElement("div");
+    var cntdownId = document.createAttribute("id");
+    var cntdownIdValue = countdownIds.pop(); 
+    cntdownId.value = "cntdown-" + cntdownIdValue;
+    cntdownDiv.setAttributeNode(cntdownId);
 
-date.addEventListener("input", function(){
-        
+    cntdownDiv.innerHTML = "<p>"+
+                            "<span class=\"years\">0</span> Y "+ 
+                            "<span class=\"months\"> 0 </span> Mo "+
+                            "<span class=\"days\"> 0 </span> D "+
+                            "<span class=\"hours\"> 0 </span> H "+
+                            "<span class=\"minutes\"> 0 </span> M "+
+                            "<span class=\"seconds\"> 0 </span> S " + 
+                            "</p>"+ 
+                            "<input type=\"date\">";
+
+    cntdownContainer.appendChild(cntdownDiv);
+
+    var cntdownDate = document.querySelector("#cntdown-" + cntdownIdValue + " input[type=date]");
+    cntdownDate.addEventListener("input", initializeCntdown);
+});
+
+function initializeCntdown(){
     console.log("Date input event fired");
-
     var splitDate = this.value.split("-");
-
     var curDate = moment(new Date());
 
     // The -1 in month is necessary because the months are zero based in Date objects
     var nextDate = moment(new Date(splitDate[0], splitDate[1] - 1, splitDate[2]));
-
     var diffArray = diffTime(curDate, nextDate);
 
-    countdown = moment.duration({
+    var cntdownIdValue = Number(this.parentElement.getAttribute("id").replace("cntdown-",""));
+
+    cntdowns[cntdownIdValue] = moment.duration({
         seconds: diffArray[5],
         minutes: diffArray[4],
         hours: diffArray[3],
@@ -31,9 +48,8 @@ date.addEventListener("input", function(){
         years: diffArray[0]
     });
 
-    setInterval(decrementTime, 1000);
-
-});
+    displayCurTime(cntdownIdValue);
+}
 
 //Accpet two dates and return an array containing the number of years, months, days, hours, minutes and seconds
 //between date1 and date2
@@ -59,13 +75,23 @@ function decrementTime(){
 }
 
 //Format the HTML in order to show the countdown time
-function displayCurTime(){
-    years.textContent = countdown.years();
-    months.textContent = countdown.months();
-    days.textContent = countdown.days();
-    hours.textContent = countdown.hours();
-    minutes.textContent = countdown.minutes();
-    seconds.textContent = countdown.seconds();
+function displayCurTime(id){
+
+    var query = "#cntdown-" + id;
+
+    var years = document.querySelector(query + " .years");
+    var months = document.querySelector(query + " .months");
+    var days = document.querySelector(query + " .days");
+    var hours = document.querySelector(query + " .hours");
+    var minutes = document.querySelector(query + " .minutes");
+    var seconds = document.querySelector(query + " .seconds");
+
+    years.textContent = cntdowns[id].years();
+    months.textContent = cntdowns[id].months();
+    days.textContent = cntdowns[id].days();
+    hours.textContent = cntdowns[id].hours();
+    minutes.textContent = cntdowns[id].minutes();
+    seconds.textContent = cntdowns[id].seconds();
 }
 
 
